583d0c71501b722ef1c5d17da21c0fe20d9bf7a0
Shah Warraich <shahjahan.w@gmail.com>
2013-04-06 12:17:05 -0400

    Adding movie display to movies.html.

--- ymcproject/movie_master/views.py
+++ ymcproject/movie_master/views.py
@@ -2,6 +2,7 @@ import datetime
 from django.views.generic import TemplateView
 from movie_master import forms
 from django.shortcuts import render
+from movies.api import get_movies
 
 
 # This is a class based view, using a generic view provided by django
@@ -10,13 +11,17 @@ class MovieSearchView(TemplateView):
 
     def post(self, request, *args, **kwargs):
         form = forms.MovieSearchForm(data=request.POST)
-        if form.is_valid():  #
+        if form.is_valid():
             search_word = form.cleaned_data.get('movie_name', None)
-            context = {'search_results': search_word}
-        else:  # Return the form with errors in the context
+            context = {'search_results': get_movies(search_word)}
+        else:
+            # Return the form with errors in the context
             context = {'form': form}
+
         return render(request, self.template_name, context)
 
     def get_context_data(self, *args, **kwargs):
         now = datetime.datetime.now()
         return {'current_date': now}
+
+
