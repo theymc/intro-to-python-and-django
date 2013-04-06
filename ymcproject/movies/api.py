9bc648a9278c96aea5068ca5edd9df54aee53502
Shah Warraich <shahjahan.w@gmail.com>
2013-04-06 12:31:39 -0400

    Fixing response retrieval from json object.

--- ymcproject/movies/api.py
+++ ymcproject/movies/api.py
@@ -23,7 +23,7 @@ def get_movies(search_term):
     }
 
     response = requests.get(search_url,params=params)
-    return response.json().get('movies', None)
+    return response.json['movies']
 
 
 def get_movie(movie_id):
