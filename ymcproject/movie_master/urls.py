1301ebac59e664900af2ff9cf5a368e65bbba2ae
Shah Warraich <shahjahan.w@gmail.com>
2013-04-13 15:29:05 -0400

    Adding movie suggestion functionality.

--- ymcproject/movie_master/urls.py
+++ ymcproject/movie_master/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from movie_master import views
 
 urlpatterns = patterns('',
+    url(r'^suggest/?$', views.MovieNightView.as_view(), name='suggest'),
     url(r'^/?$', views.MovieSearchView.as_view(), name='home'),  # A function based view
 )
